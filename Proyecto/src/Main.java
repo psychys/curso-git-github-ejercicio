a603edd720a387f156b0cc6580f42ca19c58b5fc
Galán <antoniogalanG15@gmail.com>
2021-03-07 17:07:55 +0100

    Proyecto de Java creado
    El proyecto consistirá en mostrar los participantes del curso.

---
+++ Proyecto/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio del programa.\n");
+
+        System.out.println("Miembros del curso:");
+
+        System.out.println("\nFinal del programa.");
+    }
+}
