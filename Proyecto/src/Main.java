2135494ebd0d4a3551d23e9da6f1b8af03a2cd8d
psychys <swaglord@uma.es>
2021-03-07 17:52:45 +0100

    Un trabajo muy nien hecho mi pana

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -1,15 +1,38 @@
+import java.util.*;
+
+
 public class Main {
 
+
+
+	private static void Imprimir(Map mapita){
+		
+		for(int i = 0 ; i < 5 ; i ++){
+			System.out.println(i+1 + " - " + mapita.get(i));
+		}
+	}
+
     public static void main(String[] args) {
+
+	Map<Integer,String> mapita = new HashMap<>();
+	int contador = 0;
+
         System.out.println("Inicio del programa.\n");
 
         System.out.println("Miembros del curso:");
         
-        System.out.println("Diego.");
-        System.out.println("Narta.");
-        System.out.println("pedro");
-        System.out.println("Ivan.");
-        System.out.println("Manu");
+
+        mapita.put(contador,"Diego.");
+	contador++;
+        mapita.put(contador,"Marta.");
+	contador++;
+        mapita.put(contador,"Pedro.");
+	contador++;
+        mapita.put(contador,"Ivan.");
+	contador++;
+        mapita.put(contador,"Manu.");
+
+	Imprimir(mapita);
 
         System.out.println("\nFinal del programa.");
     }
